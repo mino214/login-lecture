b0b32b421ea25edfaa0c4bf07e0dc9fc20e0d651
kangmino <kangms8283@gmail.com>
2021-05-16 01:56:44 +0900

    userStorage 모델 구현

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 "use strict";
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // 라우팅
@@ -10,8 +11,9 @@ const home = require("./src/routes/home");
 app.set("views","./src/views");
 app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`));
-
-
+app.use(express.json());
+// URL을 통해 전달되는 데이터에 한글 공백 등과 같은 문자가 포함될경우 인식되지 않는 문제 해결
+app.use(express.urlencoded({extended : true}));
 app.use("/",home); // use -> 미들 웨어 등록해주는 메서드
 module.exports = app;
 
